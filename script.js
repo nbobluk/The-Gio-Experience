375724d2dc316558645194d8a31f789cec414989
nbobluk <nbobluk@asu.edu>
2026-07-02 21:51:21 -0700

    light/dark mode, background gradient

---
+++ script.js
@@ -0,0 +1,17 @@
+"use strict";
+
+var themeToggle = document.querySelector("#themeToggle");
+
+if (themeToggle) {
+    themeToggle.addEventListener("click", function () {
+        document.body.classList.toggle("light-mode");
+
+        if (document.body.classList.contains("light-mode")) {
+            themeToggle.setAttribute("aria-pressed", "true");
+            themeToggle.setAttribute("aria-label", "Switch to dark mode");
+        } else {
+            themeToggle.setAttribute("aria-pressed", "false");
+            themeToggle.setAttribute("aria-label", "Switch to light mode");
+        }
+    });
+}
